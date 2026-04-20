d5621c111726fe3e1a5fece1d2d3f67a7629b3b4
KnTest12 <kvnguy01@gmail.com>
2026-04-20 14:31:12 -0600

    Add UI manager for player hp & current stage

---
+++ src/systems/UIManager.js
@@ -0,0 +1,30 @@
+export default class UIManager {
+  constructor(scene, player, stage) {
+    this.scene = scene;
+    this.player = player;
+    this.stage = stage;
+
+    this.hpText = this.scene.add
+      .text(40, 20, `PLAYER INTEGRITY: ${this.player.health.hp}`, {
+        fontSize: "20px",
+        color: "#00ff99",
+        fontFamily: "monospace",
+      })
+      .setDepth(10);
+
+    this.currentStageText = this.scene.add
+      .text(1050, 20, `CURRENT SECTOR: ${this.stage.currentStage + 1}`, {
+        fontSize: "20px",
+        color: "#00ff99",
+        fontFamily: "monospace",
+      })
+      .setDepth(10);
+  }
+
+  update() {
+    this.hpText.setText(`PLAYER INTEGRITY: ${this.player.health.hp}`);
+    this.currentStageText.setText(
+      `CURRENT SECTOR: ${this.stage.currentStage + 1}`,
+    );
+  }
+}
