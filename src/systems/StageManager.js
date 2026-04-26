31fac49983146ad7d142eaaaa01c2e73402e2f36
Kevin Nguyen <46980944+KnTest12@users.noreply.github.com>
2026-04-25 23:10:56 -0600

    Adjust enemy and player HP for improved gameplay balance (#25)
    * Reduce zone enemy hp
    
    * Increase player hp to allow more mistakes
    
    * Update stage 2
    
    * Update stage 6

--- src/systems/StageManager.js
+++ src/systems/StageManager.js
@@ -11,8 +11,12 @@ export default class StageManager {
         { type: "turret", col: 5, row: 2 },
       ],
       [
-        { type: "coil", col: 7, row: 0 },
-        { type: "bomb", col: 7, row: 3 },
+        { type: "turret", col: 5, row: 1, offset: 1000 },
+        { type: "turret", col: 5, row: 2 },
+        { type: "coil", col: 7, row: 1 },
+        { type: "coil", col: 7, row: 2, offset: 1500 },
+        { type: "bomb", col: 7, row: 0 },
+        { type: "bomb", col: 7, row: 3, offset: 500 },
       ],
       [
         { type: "turret", col: 4, row: 0 },
@@ -37,6 +41,10 @@ export default class StageManager {
         { type: "nova", col: 7, row: 3, offset: 2000 },
       ],
       [
+        { type: "turret", col: 6, row: 0 },
+        { type: "turret", col: 7, row: 0, offset: 1000 },
+        { type: "turret", col: 6, row: 3, offset: 2500 },
+        { type: "turret", col: 7, row: 3, offset: 3500 },
         { type: "zone", col: 6, row: 1 },
         { type: "zone", col: 6, row: 2, offset: 3000 },
         { type: "zone", col: 7, row: 1, offset: 6000 },
